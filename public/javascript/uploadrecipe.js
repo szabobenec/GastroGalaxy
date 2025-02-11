ed4c1f50947f7090a9e8be29b22d8877b3d415db
Bakrosz <bakroszabi@gmail.com>
2025-02-11 19:16:29 +0100

    uploadrecipe v1.4

--- public/javascript/uploadrecipe.js
+++ public/javascript/uploadrecipe.js
@@ -40,15 +40,12 @@ const AddHozzavalo = () => {
     const hozzavalokDiv = document.getElementById('hozzavalokDiv');
     console.log(hozzavalokDiv);
     const hozzavalok = Array.from(document.getElementsByClassName('hozzavalo'));
-    console.log(hozzavalok);
-    const innerHozzavaloDivs = Array.from(document.getElementsByClassName('innerHozzavaloDiv'));
-    console.log(innerHozzavaloDivs);
-    console.log(innerHozzavaloDivs.length);
+    console.log(hozzavalok.length);
 
-    if (innerHozzavaloDivs.length < 50) {
+    if (hozzavalok.length < 50) {
         const hozzavaloDiv = document.createElement('div');
         hozzavalokDiv.appendChild(hozzavaloDiv);
-        hozzavaloDiv.id = `hozzavalo${innerHozzavaloDivs.length + 1}`;
+        hozzavaloDiv.id = `hozzavalo${hozzavalok.length}`;
         hozzavaloDiv.setAttribute('class', 'outerHozzavalo');
         const innerHozzavaloDiv = document.createElement('div');
         hozzavaloDiv.appendChild(innerHozzavaloDiv);
@@ -60,16 +57,14 @@ const AddHozzavalo = () => {
 
         const label1 = document.createElement('label');
         nevSor.appendChild(label1);
-        label1.setAttribute('for', `hozzavaloNev${innerHozzavaloDivs.length + 1}`);
+        label1.setAttribute('for', `hozzavaloNev${hozzavalok.length}`);
         label1.innerHTML = 'Hozzávaló neve:';
         const textBox1 = document.createElement('input');
         nevSor.appendChild(textBox1);
         textBox1.type = 'text';
-        textBox1.name = `hozzavaloNev${innerHozzavaloDivs.length + 1}`;
-        textBox1.id = `hozzavaloNev${innerHozzavaloDivs.length + 1}`;
-
-        // <label for="hozzavaloNev0">Hozzávaló neve:</label>
-        // <input type="text" name="hozzavaloNev0" id="hozzavaloNev0" />
+        textBox1.name = `hozzavaloNev${hozzavalok.length}`;
+        textBox1.id = `hozzavaloNev${hozzavalok.length}`;
+        textBox1.setAttribute('class', 'hozzavaloNev');
 
         const mennyisegSor = document.createElement('div');
         innerHozzavaloDiv.appendChild(mennyisegSor);
@@ -77,14 +72,31 @@ const AddHozzavalo = () => {
 
         const label2 = document.createElement('label');
         mennyisegSor.appendChild(label2);
-        label2.setAttribute('for', `mennyisegNev${innerHozzavaloDivs.length + 1}`);
+        label2.setAttribute('for', `mennyisegNev${hozzavalok.length}`);
         label2.innerHTML = 'Mennyisége:';
         const textBox2 = document.createElement('input');
         mennyisegSor.appendChild(textBox2);
         textBox2.type = 'text';
-        textBox2.name = `mennyisegNev${innerHozzavaloDivs.length + 1}`;
-        textBox2.id = `mennyisegNev${innerHozzavaloDivs.length + 1}`;
+        textBox2.name = `mennyisegNev${hozzavalok.length}`;
+        textBox2.id = `mennyisegNev${hozzavalok.length}`;
+        textBox1.setAttribute('class', 'hozzavaloMennyiseg');
+
+        const btnDiv = document.createElement('div');
+        hozzavaloDiv.appendChild(btnDiv);
+        btnDiv.setAttribute('class', 'btnDiv');
+        const btn = document.createElement('input');
+        btnDiv.appendChild(btn);
+        btn.type = 'button';
+        btn.id = `minus${hozzavalok.length}`;
+        btn.value = '-';
+        btn.addEventListener('click', () => {
+            RemoveHozzavalo(hozzavalokDiv);
+        });
     }
+};
+
+const RemoveHozzavalo = (div) => {
+    div.removeChild(div.lastChild);
 };
 
 const AddLepes = () => {
@@ -92,4 +104,34 @@ const AddLepes = () => {
     console.log(elkeszitesDiv);
     const lepesek = Array.from(document.getElementsByClassName('lepes'));
     console.log(lepesek);
+
+    if (lepesek.length < 10) {
+        const lepesDiv = document.createElement('div');
+        elkeszitesDiv.appendChild(lepesDiv);
+        lepesDiv.id = `elkeszites${lepesek.length}`;
+        lepesDiv.setAttribute('class', 'lepes');
+
+        const lepesSor = document.createElement('div');
+        lepesDiv.appendChild(lepesSor);
+        lepesSor.setAttribute('class', 'lepesSor');
+
+        const label = document.createElement('label');
+        lepesSor.appendChild(label);
+        label.setAttribute('for', `lepes${lepesek.length}`);
+        label.innerHTML = `${lepesek.length + 1}. lépés:`;
+        const textArea = document.createElement('textarea');
+        lepesSor.appendChild(textArea);
+        textArea.name = `lepes${lepesek.length}`;
+        textArea.id = `lepes${lepesek.length}`;
+
+        // <label for="lepes0">1. lépés:</label>
+        // <textarea name="lepes0" id="lepes0"></textarea>
+    }
+    document.getElementById('elkeszitesMinus').addEventListener('click', () => {
+        RemoveLepes(elkeszitesDiv);
+    });
+};
+
+const RemoveLepes = (div) => {
+    div.removeChild(div.lastChild);
 };
