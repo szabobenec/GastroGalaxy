bda5bb06b5e2c504a52eaef07c057c21edf0aa77
Bakrosz <bakroszabi@gmail.com>
2025-02-22 13:23:33 +0100

    Update receptek.sql

--- sql/receptek.sql
+++ sql/receptek.sql
@@ -4,28 +4,28 @@ COLLATE utf8_hungarian_ci;
 
 USE receptek;
 
-CREATE TABLE recept (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipus VARCHAR(25) NOT NULL,
-    nev VARCHAR(100) NOT NULL,
-    tagid INT,
-    ido INT,
-    adag INT NOT NULL,
-    hozzavaloid INT NOT NULL,
-    elkeszites TEXT NOT NULL,
-    kepnev VARCHAR(100),
-    forras VARCHAR(255)
+-- CREATE TABLE recept (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     tipus VARCHAR(25) NOT NULL,
+--     nev VARCHAR(100) NOT NULL,
+--     tagid INT,
+--     ido INT,
+--     adag INT NOT NULL,
+--     hozzavaloid INT NOT NULL,
+--     elkeszites TEXT NOT NULL,
+--     kepnev VARCHAR(100),
+--     forras VARCHAR(255)
 
-    FOREIGN KEY REFERENCES (recept.tagid = tag.id)
-    FOREIGN KEY REFERENCES (recept.hozzavaloid = hozzavalo.id)
-);
+--     FOREIGN KEY (tagid) REFERENCES tag(id)
+--     FOREIGN KEY (hozzavaloid) REFERENCES hozzavalo(id)
+-- );
 
-CREATE TABLE tag (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tagek JSON
-);
+-- CREATE TABLE tag (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     tagek JSON
+-- );
 
-CREATE TABLE hozzavalo (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    hozzavalok JSON
-);
+-- CREATE TABLE hozzavalo (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     hozzavalok JSON
+-- );
