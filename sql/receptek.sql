e77c9552b3ac97b53be9a03b01ad81ad44b390c2
Bakrosz <bakroszabi@gmail.com>
2025-02-26 14:52:52 +0100

    Update receptek.sql

--- sql/receptek.sql
+++ sql/receptek.sql
@@ -4,21 +4,18 @@ COLLATE utf8_hungarian_ci;
 
 USE receptek;
 
--- CREATE TABLE recept (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     tipus VARCHAR(25) NOT NULL,
---     nev VARCHAR(100) NOT NULL,
---     tagid INT,
---     ido INT,
---     adag INT NOT NULL,
---     hozzavaloid INT NOT NULL,
---     elkeszites TEXT NOT NULL,
---     kepnev VARCHAR(100),
---     forras VARCHAR(255)
-
---     FOREIGN KEY (tagid) REFERENCES tag(id)
---     FOREIGN KEY (hozzavaloid) REFERENCES hozzavalo(id)
--- );
+CREATE TABLE recept (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tipus VARCHAR(25) NOT NULL,
+    nev VARCHAR(100) NOT NULL,
+    tagek TEXT,
+    ido INT,
+    adag INT NOT NULL,
+    hozzavalok JSON NOT NULL,
+    elkeszites TEXT NOT NULL,
+    kepnev VARCHAR(100),
+    forras VARCHAR(255)
+);
 
 -- CREATE TABLE tag (
 --     id INT PRIMARY KEY AUTO_INCREMENT,
