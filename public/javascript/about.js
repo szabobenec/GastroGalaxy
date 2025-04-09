72dc321a03159ce0671301be7612da488ef4007f
Bakrosz <bakroszabi@gmail.com>
2025-04-09 23:21:01 +0200

    about fix v2143uy314

--- public/javascript/about.js
+++ public/javascript/about.js
@@ -1,3 +1,37 @@
+//! GET metódus
+const getAPI = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    reject(`Hiba: ${response.statusText} (${response.status})`);
+                }
+                return response.json();
+            })
+            .then((data) => resolve(data))
+            .catch((error) => reject(`Hiba történt: ${error.message}`));
+    });
+};
+
+//! POST metódus
+const postAPI = (url, postObject) => {
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(postObject)
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    reject(`Hiba: ${response.statusText} (${response.status})`);
+                }
+                return response.json();
+            })
+            .then((data) => resolve(data))
+            .catch((error) => reject(`Hiba: ${error}`));
+    });
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
     console.log('Welcome to the About page!');
 
