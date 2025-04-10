c75d791eb6533819d62b1b4fb2a733592e4e53ac
Bakrosz <bakroszabi@gmail.com>
2025-04-10 22:54:36 +0200

    admin v0.6

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -219,24 +219,139 @@ const ShowData = async () => {
     nevI.placeholder = 'név';
     nevI.setAttribute('class', 'adatInput');
 
-    //! TIPUS
     //? tipus
+    const tipusDiv = document.createElement('div');
+    adatokFormData.appendChild(tipusDiv);
+    tipusDiv.setAttribute('class', 'formDiv');
+    const tipusL = document.createElement('label');
+    tipusDiv.appendChild(tipusL);
+    tipusL.setAttribute('for', 'tipus');
+    tipusL.innerHTML = 'Típus:';
+    const tipusSelect = document.createElement('select');
+    tipusDiv.appendChild(tipusSelect);
+    tipusSelect.setAttribute('class', 'tipusSelect');
+    tipusSelect.id = 'tipus';
+    const option1 = document.createElement('option');
+    tipusSelect.appendChild(option1);
+    option1.value = 'reggeli';
+    option1.innerHTML = 'reggeli';
+    const option2 = document.createElement('option');
+    tipusSelect.appendChild(option2);
+    option2.value = 'leves';
+    option2.innerHTML = 'leves';
+    const option3 = document.createElement('option');
+    tipusSelect.appendChild(option3);
+    option3.value = 'főétel';
+    option3.innerHTML = 'főétel';
+    const option4 = document.createElement('option');
+    tipusSelect.appendChild(option4);
+    option4.value = 'desszert';
+    option4.innerHTML = 'desszert';
+    const option5 = document.createElement('option');
+    tipusSelect.appendChild(option5);
+    option5.value = 'egyéb';
+    option5.innerHTML = 'egyéb';
 
     //? elkeszitesi ido
-    const nevDiv = document.createElement('div');
-    adatokFormData.appendChild(nevDiv);
-    nevDiv.setAttribute('class', 'formDiv');
-    const nevL = document.createElement('label');
-    nevDiv.appendChild(nevL);
-    nevL.setAttribute('for', 'nev');
-    nevL.innerHTML = 'Recept neve:';
-    const nevI = document.createElement('input');
-    nevDiv.appendChild(nevI);
-    nevI.type = 'text';
-    nevI.id = 'nev';
-    nevI.name = 'nev';
-    nevI.placeholder = 'név';
-    nevI.setAttribute('class', 'adatInput');
+    const idoDiv = document.createElement('div');
+    adatokFormData.appendChild(idoDiv);
+    idoDiv.setAttribute('class', 'formDiv');
+    const idoL = document.createElement('label');
+    idoDiv.appendChild(idoL);
+    idoL.setAttribute('for', 'ido');
+    idoL.innerHTML = 'Elkészítési idő (perc):';
+    const idoI = document.createElement('input');
+    idoDiv.appendChild(idoI);
+    idoI.type = 'text';
+    idoI.id = 'ido';
+    idoI.name = 'ido';
+    idoI.placeholder = 'idő';
+    idoI.setAttribute('class', 'adatInput');
+
+    //? adag
+    const adagDiv = document.createElement('div');
+    adatokFormData.appendChild(adagDiv);
+    adagDiv.setAttribute('class', 'formDiv');
+    const adagL = document.createElement('label');
+    adagDiv.appendChild(adagL);
+    adagL.setAttribute('for', 'adag');
+    adagL.innerHTML = 'Adag (fő):';
+    const adagI = document.createElement('input');
+    adagDiv.appendChild(adagI);
+    adagI.type = 'text';
+    adagI.id = 'adag';
+    adagI.name = 'adag';
+    adagI.placeholder = 'adag';
+    adagI.setAttribute('class', 'adatInput');
+
+    //? tagek
+    const tagDiv = document.createElement('div');
+    adatokFormData.appendChild(tagDiv);
+    tagDiv.setAttribute('class', 'formDiv');
+    const tagL = document.createElement('label');
+    tagDiv.appendChild(tagL);
+    tagL.setAttribute('for', 'tagek');
+    tagL.innerHTML = `Tagek (';'-el elválasztva):`;
+    const tagI = document.createElement('input');
+    tagDiv.appendChild(tagI);
+    tagI.type = 'text';
+    tagI.id = 'tagek';
+    tagI.name = 'tagek';
+    tagI.placeholder = 'tagek';
+    tagI.setAttribute('class', 'adatInput');
+
+    //? hozzavalok
+    const hozzavaloCim = document.createElement('h3');
+    adatokFormData.appendChild(hozzavaloCim);
+    hozzavaloCim.innerHTML = 'Hozzávalók';
+    const hozzavalokDiv = document.createElement('div');
+    adatokFormData.appendChild(hozzavalokDiv);
+    hozzavalokDiv.setAttribute('class', 'formDivHozzavalo');
+    hozzavalokDiv.id = 'hozzavalokDiv';
+
+    //? elkeszites
+    const lepesCim = document.createElement('h3');
+    adatokFormData.appendChild(lepesCim);
+    lepesCim.innerHTML = 'Lépések';
+    const lepesDiv = document.createElement('div');
+    adatokFormData.appendChild(lepesDiv);
+    lepesDiv.setAttribute('class', 'formDivLepes');
+    lepesDiv.id = 'lepesDiv';
+
+    //? forras
+    const forrasDiv = document.createElement('div');
+    adatokFormData.appendChild(forrasDiv);
+    forrasDiv.setAttribute('class', 'formDiv');
+    const forrasL = document.createElement('label');
+    forrasDiv.appendChild(forrasL);
+    forrasL.setAttribute('for', 'forras');
+    forrasL.innerHTML = 'Recept forrása:';
+    const forrasI = document.createElement('input');
+    forrasDiv.appendChild(forrasI);
+    forrasI.type = 'text';
+    forrasI.id = 'forras';
+    forrasI.name = 'forras';
+    forrasI.placeholder = 'forrás';
+    forrasI.setAttribute('class', 'adatInput');
+
+    //? gombok
+    const buttonDiv = document.createElement('div');
+    adatokFormData.appendChild(buttonDiv);
+    buttonDiv.setAttribute('class', 'formDivButtons');
+    const updateBtn = document.createElement('input');
+    buttonDiv.appendChild(updateBtn);
+    updateBtn.type = 'button';
+    updateBtn.id = 'updateBtn';
+    updateBtn.name = 'updateBtn';
+    updateBtn.value = 'Szerkesztés';
+    updateBtn.setAttribute('class', 'buttons');
+    const deleteBtn = document.createElement('input');
+    buttonDiv.appendChild(deleteBtn);
+    deleteBtn.type = 'button';
+    deleteBtn.id = 'deleteBtn';
+    deleteBtn.name = 'deleteBtn';
+    deleteBtn.value = 'Törlés';
+    deleteBtn.setAttribute('class', 'buttons');
 };
 
 //! Receptek abc sorrendbe rendezése, az egyszerűbb megtalálás érdekében
@@ -314,4 +429,71 @@ const SearchRecipe = (elem, data) => {
 
 const ManageRecipe = (data) => {
     console.log(data);
+    const formData = new FormData(document.getElementById('adatokFormData'));
+    console.log(...formData);
+
+    for (let [key, value] of formData) {
+        formData.set(key, data[key]);
+        document.getElementById(key).value = data[key];
+    }
+
+    document.getElementById('tipus').value = data.tipus;
+    formData.append('tipus', data.tipus);
+
+    const hozzavalokDiv = document.getElementById('hozzavalokDiv');
+    hozzavalokDiv.innerHTML = '';
+
+    const hozzavalok = JSON.parse(data.hozzavalok);
+    console.log(hozzavalok);
+
+    for (let item in hozzavalok) {
+        const hozzavaloDiv = document.createElement('div');
+        hozzavalokDiv.appendChild(hozzavaloDiv);
+        hozzavaloDiv.setAttribute('class', 'formDiv');
+        const hozzavaloI = document.createElement('input');
+        hozzavaloDiv.appendChild(hozzavaloI);
+        hozzavaloI.type = 'text';
+        hozzavaloI.id = `hozzavalo-${item}`;
+        hozzavaloI.name = `hozzavalo-${item}`;
+        hozzavaloI.placeholder = 'hozzávaló';
+        hozzavaloI.setAttribute('class', 'hozzavaloInput hozzavalo');
+        hozzavaloI.value = item;
+        formData.set(`hozzavalo-${item}`, item);
+        const hozzavaloP = document.createElement('div');
+        hozzavaloDiv.appendChild(hozzavaloP);
+        hozzavaloP.innerHTML = ':';
+        const mennyisegI = document.createElement('input');
+        hozzavaloDiv.appendChild(mennyisegI);
+        mennyisegI.type = 'text';
+        mennyisegI.id = `mennyiseg-${item}`;
+        mennyisegI.name = `mennyiseg-${item}`;
+        mennyisegI.placeholder = 'mennyiség';
+        mennyisegI.setAttribute('class', 'hozzavaloInput mennyiseg');
+        mennyisegI.value = hozzavalok[item];
+        formData.set(`mennyiseg-${item}`, hozzavalok[item]);
+    }
+
+    const lepesDiv = document.getElementById('lepesDiv');
+    lepesDiv.innerHTML = '';
+
+    const lepesek = data.elkeszites;
+
+    for (let item of lepesek.split('\n')) {
+        const lepes = document.createElement('textArea');
+        lepesDiv.appendChild(lepes);
+        lepes.placeholder = 'Lépés leírása';
+        lepes.setAttribute('class', 'lepes');
+        lepes.id = `lepes-${item.slice(0, 1)}`;
+        const text = item.slice(3);
+        lepes.innerHTML = text;
+    }
+
+    document.getElementById('updateBtn').addEventListener('click', UpdateRecipe);
+    document.getElementById('deleteBtn').addEventListener('click', DeleteRecipe);
+
+    console.log(...formData);
 };
+
+const UpdateRecipe = () => {};
+
+const DeleteRecipe = () => {};
